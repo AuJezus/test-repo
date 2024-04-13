a88535899e48f47cfa297a93171d9117d9cfc10f
aujezus <augispay@gmail.com>
2024-04-13 08:43:14 +0300

    test

--- helloWorld.js
+++ helloWorld.js
@@ -10,3 +10,9 @@ console.log("Hello World");
 console.log("Hello World");
 console.log("Hello World");
 console.log("Hello World");
+
+const commits = activity?.events?.reduce(
+  (acc, e) => acc + (e.payload.size || 0),
+  0
+);
+return commits || 0;
